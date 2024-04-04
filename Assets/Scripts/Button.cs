10926bd4a7f6d4b8c1ecb6cc6fe6ce74d80b52e0
AKarim04 <165771953+AKarim04@users.noreply.github.com>
2024-04-04 16:43:45 +0200

    Skapat olika skript för att hantera lås i spelet

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public int num;
+
+    [ContextMenu("Press")]
+
+    private void Press()
+    {
+
+    }
+
+}

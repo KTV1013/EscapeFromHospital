04c223aefd2008784b6e120e9bebe39f48908561
Toni Videgren <71024505+KTV1013@users.noreply.github.com>
2024-04-08 11:11:51 +0200

    Update Interactible.cs

--- Assets/Scripts/Interactible.cs
+++ Assets/Scripts/Interactible.cs
@@ -8,19 +8,39 @@ public class Interactible : MonoBehaviour
     [SerializeField]
     protected Transform cameraTransform;
     [SerializeField]
+    protected Transform RotationPoint;
+    [SerializeField]
     protected Transform playerPosition;
-
+    [SerializeField]
+    Interactible parentInteractible;
     protected Camera playerCamera;
     private void Start()
     {
         playerCamera = Camera.main;
     }
+
     public virtual void StartInteraction() 
     {
         playerCamera.transform.SetParent(cameraTransform);
     }
-    public virtual void EndInteraction() 
+    public virtual void EndInteraction()
     {
-        //Todo give main camera back to player
+        if (parentInteractible != null) 
+        {
+            parentInteractible.StartInteraction();
+        }
+        else
+        {
+            //Todo give main camera back to player
+        }
+    }
+
+    protected IEnumerator LerpCamera()
+    {
+        while (playerCamera.transform.localPosition != Vector3.zero)
+        {
+            Vector3.Lerp(playerCamera.transform.position, Vector3.zero, 0.3f);
+            yield return null;
+        }
     }
 }

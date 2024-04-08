872a48a6bca94059b86456ed4024f3da5bd085e4
Toni Videgren <71024505+KTV1013@users.noreply.github.com>
2024-04-08 11:58:04 +0200

    Interactible Camera Transition Works

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0f, 2f)]
+    float Speed = 1.8f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition != Vector3.zero)
+        {
+            Vector3 pos = transform.localPosition;
+            pos = Vector3.Lerp(pos, Vector3.zero, Speed * Time.deltaTime);
+            transform.localPosition = pos;
+        }
+        if (transform.localRotation != Quaternion.identity)
+        {
+            Quaternion rot = transform.localRotation;
+            rot =Quaternion.Slerp(rot, Quaternion.identity, Speed * Time.deltaTime);
+            transform.localRotation = rot;
+        }
+    }
+
+    public void ResetParent()
+    {
+
+    }
+}
--- Assets/Scripts/Interactible.cs
+++ Assets/Scripts/Interactible.cs
@@ -8,20 +8,18 @@ public class Interactible : MonoBehaviour
     [SerializeField]
     protected Transform cameraTransform;
     [SerializeField]
-    protected Transform RotationPoint;
-    [SerializeField]
-    protected Transform playerPosition;
-    [SerializeField]
     Interactible parentInteractible;
+
     protected Camera playerCamera;
+    
     private void Start()
     {
         playerCamera = Camera.main;
     }
-
+    [ContextMenu("StartInteraction")]
     public virtual void StartInteraction() 
     {
-        playerCamera.transform.SetParent(cameraTransform);
+        playerCamera.transform.SetParent(cameraTransform, true);
     }
     public virtual void EndInteraction()
     {
@@ -32,15 +30,6 @@ public class Interactible : MonoBehaviour
         else
         {
             //Todo give main camera back to player
-        }
-    }
-
-    protected IEnumerator LerpCamera()
-    {
-        while (playerCamera.transform.localPosition != Vector3.zero)
-        {
-            Vector3.Lerp(playerCamera.transform.position, Vector3.zero, 0.3f);
-            yield return null;
         }
     }
 }

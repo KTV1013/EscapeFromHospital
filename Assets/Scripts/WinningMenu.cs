e883160a0d009273c81fcfc0335163c5dad8e628
AKarim04 <165771953+AKarim04@users.noreply.github.com>
2024-04-09 10:56:32 +0200

    Skapat skript för Winning menu

---
+++ Assets/Scripts/WinningMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningMenu : MonoBehaviour
+{
+    public Canvas winningCanvas;
+
+    private void Awake()
+    {
+        winningCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+}

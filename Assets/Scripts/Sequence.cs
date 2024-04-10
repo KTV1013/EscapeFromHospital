2432d1f2a362d45539fcc3d22a41e4ffdbb0aaec
Toni Videgren <71024505+KTV1013@users.noreply.github.com>
2024-04-10 16:34:57 +0200

    Sequence Works

--- Assets/Scripts/Sequence.cs
+++ Assets/Scripts/Sequence.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,44 +8,71 @@ public class Sequence : MonoBehaviour
     [SerializeField]
     bool orderedSequence;
     [SerializeField]
-    protected List<Step> sequences;
+    protected List<StepInfo> stepInfos;
     
     protected Sequence parent;
-    public class Step 
+    [Serializable]
+    public class StepInfo 
     {
         public Sequence step;
         public bool completedSequence;
     }
-
+    int StepIndex = 0;
     protected void Start()
     {
-        foreach (var sequence in sequences) 
+        foreach (var sequence in stepInfos) 
         {
             sequence.completedSequence = false;
             sequence.step.parent = this;
         }
     }
 
-    public void CompleteStep(Sequence sequence)
+    public void CompleteStep(Sequence step)
     {
-        int index = sequences.FindIndex(seq  => seq.step == sequence);
-        if (orderedSequence) 
+        if (SequencesComplete()) return;
+        int index = stepInfos.FindIndex(stepInfo  => stepInfo.step == step);
+        if (index == -1) return;
+        int lastIndex = stepInfos.FindLastIndex(stepInfo => stepInfo.step == step);
+        
+        if (lastIndex != index)
         {
-            int failIndex = sequences.FindIndex(seq => seq.completedSequence == false);
-            if (failIndex < index)
+            bool ordered = true;
+            for (int i = index; i <= lastIndex; i++) 
             {
-                ResetProgress();
-                return;
+                bool stepDone = stepInfos[i].completedSequence;
+                if (stepInfos[i].step == step)
+                {
+                    bool unOrdered =  !ordered || i != StepIndex;
+                    
+                    if (!stepDone)
+                    {
+                        if (orderedSequence && unOrdered) 
+                        {
+                            ResetProgress();
+                            return;
+                        }
+                        index = i;
+                        break;
+                    }
+                }
+                ordered = ordered && stepDone;
             }
         }
-        sequences[index].completedSequence = true;
+        else if (orderedSequence && index != StepIndex)
+        {
+            ResetProgress();
+            return;
+        }
+
         
+        stepInfos[index].completedSequence = true;
+        StepIndex++;
         if (SequencesComplete())
         {
             CompleteSequence();
         }
     }
-    void CompleteSequence()
+    public void CompleteSequence()
     {
         if (parent != null)
             parent.CompleteStep(this);
@@ -53,19 +81,21 @@ public class Sequence : MonoBehaviour
 
     void ResetProgress()
     {
-        foreach(var sequence in sequences)
+        Debug.Log("Reset");
+        StepIndex = 0;
+        foreach(var stepInfo in stepInfos)
         {
-            sequence.completedSequence = false;
+            stepInfo.completedSequence = false;
         }
     }
 
     protected virtual void End() 
     {
-        
+        Debug.Log("Done: " + name);
     }
 
     public bool SequencesComplete()
     {
-        return !sequences.Exists(seq => seq.completedSequence == false);
+        return !stepInfos.Exists(stepInfo => stepInfo.completedSequence == false);
     }
 }

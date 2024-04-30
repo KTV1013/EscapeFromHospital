9a1074868c6e2bd657bd547a31879360d33a9861
AKarim04 <165771953+AKarim04@users.noreply.github.com>
2024-04-30 11:50:29 +0200

    gjorde färdigt code lock pussel

--- Assets/Scripts/CodeLockPuzzle.cs
+++ Assets/Scripts/CodeLockPuzzle.cs
@@ -5,18 +5,17 @@ using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 
-public class LockPuzzle : MonoBehaviour
+public class CodeLockPuzzle : MonoBehaviour
 {
     Ray ray;
     public GameObject gear1, gear2, gear3, gear4;
-    [SerializeField] float gear1Rot , gear2Rot, gear3Rot, gear4Rot;
+    [SerializeField] Quaternion gear1Rot, gear2Rot, gear3Rot, gear4Rot;
     [SerializeField] bool puzzleSolved = false;
     private float difference = 0.1f;
-    
 
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -26,7 +25,7 @@ public class LockPuzzle : MonoBehaviour
             ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
             shootRay();          
         }
-        //CodeCheck();
+        CodeCheck();
     }
 
     void shootRay()
@@ -34,29 +33,23 @@ public class LockPuzzle : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hitInfo))
         {
             if (hitInfo.collider.CompareTag("Gear"))
-            {
+            {                
                 hitInfo.collider.gameObject.transform.Rotate(new Vector3(36f,0f,0f));
-
             }
         }
     }
 
-    //void CodeCheck()
-    //{
+    void CodeCheck()
+    {
 
-    //    if (gear1.transform.rotation.x == gear1Rot)
-    //    {
-    //        if (gear2.transform.rotation.x == gear2Rot)
-    //        {
-    //            if ((gear3.transform.rotation.x == gear3Rot))
-    //            {
-    //                if (gear4.transform.rotation.x == gear4Rot)
-    //                {
-    //                    puzzleSolved = true;
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
+        if (Quaternion.Angle(gear1.transform.rotation, gear1Rot) < difference && 
+            Quaternion.Angle(gear2.transform.rotation, gear2Rot) < difference &&
+            Quaternion.Angle(gear3.transform.rotation, gear3Rot) < difference &&
+            Quaternion.Angle(gear4.transform.rotation, gear4Rot) < difference)
+        {
+            puzzleSolved = true;
+        }
+    }
+
 
 }

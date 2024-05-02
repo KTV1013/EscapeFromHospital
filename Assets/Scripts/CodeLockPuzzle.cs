4cd03aa0ac87ac2cd557cdfe91796385df50b49a
Abdulakder Alkhayat <Abaa22@student.bth.se>
2024-05-02 17:15:47 +0200

    Fixing the animation scripts for room 1

--- Assets/Scripts/CodeLockPuzzle.cs
+++ Assets/Scripts/CodeLockPuzzle.cs
@@ -5,14 +5,18 @@ using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CodeLockPuzzle : MonoBehaviour
 {
     //RayCasting rayCasting;
+    public Animator AnimatorForLock;
+    public Animator AnimatorForLockerDoor; 
     public GameObject gear1, gear2, gear3, gear4;
     [SerializeField] Quaternion gear1Rot, gear2Rot, gear3Rot, gear4Rot;
     [SerializeField] bool puzzleSolved = false;
     private float difference = 0.1f;
+    public GameObject lockObject;
 
     void Start()
     {
@@ -21,19 +25,17 @@ public class CodeLockPuzzle : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            //rayCasting.ShootRay();
-            CodeCheck();
-        }
+        CodeCheck();
+        LockOpening();
+        LockerDoorOpening();
     }
 
     void CodeCheck()
     {
-        if ((Quaternion.Angle(gear1.transform.rotation, gear1Rot) < difference)) { Debug.Log("Gear1 done"); }
-        if ((Quaternion.Angle(gear2.transform.rotation, gear2Rot) < difference)) { Debug.Log("Gear2 done"); }
-        if ((Quaternion.Angle(gear3.transform.rotation, gear3Rot) < difference)) { Debug.Log("Gear3 done"); }
-        if ((Quaternion.Angle(gear4.transform.rotation, gear4Rot) < difference)) { Debug.Log("Gear4 done"); }
+        //if ((Quaternion.Angle(gear1.transform.rotation, gear1Rot) < difference)) { Debug.Log("Gear1 done"); }
+        //if ((Quaternion.Angle(gear2.transform.rotation, gear2Rot) < difference)) { Debug.Log("Gear2 done"); }
+        //if ((Quaternion.Angle(gear3.transform.rotation, gear3Rot) < difference)) { Debug.Log("Gear3 done"); }
+        //if ((Quaternion.Angle(gear4.transform.rotation, gear4Rot) < difference)) { Debug.Log("Gear4 done"); }
 
         if (Quaternion.Angle(gear1.transform.rotation, gear1Rot) < difference &&
             Quaternion.Angle(gear2.transform.rotation, gear2Rot) < difference &&
@@ -41,13 +43,34 @@ public class CodeLockPuzzle : MonoBehaviour
             Quaternion.Angle(gear4.transform.rotation, gear4Rot) < difference)
         {
             puzzleSolved = true;
-            Debug.Log("Puzzle Solved!");
+            //Debug.Log("Puzzle Solved!");
         }
 
     }
 
-    public bool CodeStatus ()
+    public bool GetCodeStatus ()
     {
         return puzzleSolved;
+    }
+
+    private void LockOpening()
+    {
+        if (puzzleSolved == true)
+        {
+            AnimatorForLock.SetBool("IsLocked", true);
+            StartCoroutine(LockerDoorOpening());
+
+        }
+    }
+
+    private IEnumerator LockerDoorOpening()
+    {
+        if (puzzleSolved == true)
+        {
+            Debug.Log("Waiting 2 seconds");
+            yield return new WaitForSeconds(1);
+            AnimatorForLockerDoor.SetBool("IsClosed", true);
+            lockObject.transform.parent = AnimatorForLockerDoor.transform;
+        }
     }
 }

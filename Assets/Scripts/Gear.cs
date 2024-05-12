f42b2cc2ab7020c67bca341feb03307d9fbbc336
Toni Videgren <71024505+KTV1013@users.noreply.github.com>
2024-05-12 12:05:51 +0200

    gear

---
+++ Assets/Scripts/Gear.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : Interactable
+{
+    AudioManager audioManager;
+    private void Start()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+    public override void EndInteraction()
+    {
+        
+    }
+
+    public override void StartInteraction()
+    {
+        RotateGear();
+    }
+
+    // Funktion för att rotarea Gear när man träffar den med en ray
+    private void RotateGear()
+    {
+        transform.Rotate(new Vector3(-36f, 0f, 0f));
+        audioManager.PlaySFX(audioManager.GearSound);
+        Debug.Log("Roterad");
+    }
+}

faf47f12b8bce62e303726b2ec397077de3aea17
Quasar <abdabd0212@users.noreply.github.com>
2024-05-19 15:27:06 +0200

    my scripts

---
+++ Assets/Scripts/Abdullah/DesplayItem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DesplayItem : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject itemOnView;
+    public string itemNmae;
+    float speed = 500;
+    public bool canRotate = false;
+    public GameObject cameraFieldOfView;
+    float fieldOfView = 60;
+
+    private void Update()
+    {
+
+        if (item != null)
+        {
+            item.transform.position = gameObject.transform.position;
+        }
+        
+        if(Input.GetMouseButton(0) && itemOnView != null && canRotate)
+        {
+            float MouseXAxis = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+            float MouseYAxis = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+            itemOnView.transform.Rotate(Vector3.up, -MouseXAxis, Space.World);
+            itemOnView.transform.Rotate(Vector3.right, MouseYAxis, Space.World);
+        }
+
+        if(canRotate)
+        {
+            cameraFieldOfView.GetComponent<Camera>().fieldOfView = fieldOfView;
+            fieldOfView += Input.mouseScrollDelta.y * 2;
+            if (Input.GetKeyDown(KeyCode.Tab))
+                fieldOfView = 60;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab) && item != null)
+        {
+            Destroy(item);
+            canRotate = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Abdullah/XRayScreenImage.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRayScreenImage : MonoBehaviour
+{
+    public Material screenOff;
+    public Material screenOn;
+    public Material hand;
+    public Material handWithKey;
+    public Renderer render;
+
+    void Start()
+    {
+        render = gameObject.GetComponent<Renderer>();
+        render.material = screenOff;
+    }
+
+    public void ActivateXRayMachine()
+    {
+        render.material = screenOn;
+    }
+
+    public void GetManiuMaterial()
+    {
+        render.material = screenOn;
+    }
+
+    public void SetImage(GameObject item)
+    {
+        if (item.GetComponent<HasKey>())
+        {
+            render.material = handWithKey;
+        }
+        else
+        {
+            render.material = hand;
+        }
+    }
+}
